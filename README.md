a169067b2d1f11228a79e0dcdae26311ea1abacb
clajo <claes.w.johansson@gmail.com>
2015-08-23 23:35:09 +0200

    update

--- README.md
+++ README.md
@@ -12,10 +12,8 @@ https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Datas
 
 Download the data and set the folder 'UCI HAR Dataset' as your working directory.
 
-The R code in run_analysis.R combines training and test data, creating a tidy dataset with the average of each variable for each activity and each subject.
-Only mean and std variables from the data sourses are used.
-
-The result is stored into the textfile grouped_tidydata.txt.
+This R code in run_analysis.R reads data from the 'UCI HAR Dataset'. It combine training and test data, transfors it into a data frame with one row per subject and activity, calculates mean over all mean() or std() variables.
+The final data frame is exported to grouped_tidydata.txt.
 
 
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
-#This script reads data from 'UCI HAR Dataset' and transfors it into a data frame 
-#with one row per subject and activity and calculats mean values for all value
-#columns with names including mean() or std()
+# This R code in run_analysis.R reads data from the 'UCI HAR Dataset'. 
+# It combine training and test data, transfors it into a data frame 
+# with one row per subject and activity, calculates mean over all mean() and std() variables.
+#The final data frame is exported to grouped_tidydata.txt.
 
 #The folder 'UCI HAR Dataset' should be set as working directory.
 
@@ -22,7 +23,7 @@ data_test<-data.frame(subj_test,y_test,x_test)
 
 
 
-#--train data-----------------------------------------
+#--training data-----------------------------------------
 
 #read X_train data
 x_train<-read.table("train/X_train.txt")
@@ -37,7 +38,7 @@ data_train<-data.frame(subj_train,y_train,x_train)
 
 #--concatenated data----------------------------------
 
-#Concatinate data_test and data_train into trans
+#Concatinate data_test and data_train into data 
 data<-rbind(data_test,data_train)
 
 #Read in features
@@ -51,11 +52,11 @@ activity<-read.table("activity_labels.txt")
 #Add column names to Activity
 colnames(activity)<-c("activity_code","activity")
 
-#Merge activity names to trans and select subject, activity, columns with name including mean() or std()
+#Merge activity names to data and select subject, activity, columns with names including mean() or std()
 data<-merge(activity,data)%>%select(subject,activity,matches("mean()"),matches("std()"))
 
 #Group data by subject and activity and use function mean over all value columns
 grouped_tidydata<-data %>% group_by(subject,activity) %>% summarise_each(funs(mean))
 
-#export data frame grouped to grouped_data.txt
+#export data frame grouped to grouped_tidydata.txt
 write.table(grouped_tidydata,file="grouped_tidydata.txt",row.names = FALSE)
